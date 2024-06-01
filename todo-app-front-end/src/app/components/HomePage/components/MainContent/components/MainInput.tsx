8e52371fa7ccc2d07216e68a37869564a37bebb2
YasserRABIE <yasserrabie110306@gmail.com>
2024-06-01 19:06:12 +0300

    Refactor MainInput component and add TasksContainer component

--- todo-app-front-end/src/app/components/HomePage/components/MainContent/components/MainInput.tsx
+++ todo-app-front-end/src/app/components/HomePage/components/MainContent/components/MainInput.tsx
@@ -1,17 +1,94 @@
 import TaskFilter from "@/app/reusable/TaskFilter";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ChangeEvent, KeyboardEvent, useEffect, useState } from "react";
+import { TaskType } from "../interfaces/Task";
+import { MakeRequest } from "@/app/scripts/makeRequest";
+import { GetToken } from "@/app/scripts/auth";
 
-function MainInput() {
+function MainInput({ setTasks, tasks }: { setTasks: Function; tasks: TaskType[] }) {
+   const [taskFilter, setTaskFilter] = useState({ color: "#3fd4f4", filter: "personal" });
+   const [taskTitle, setTaskTitle] = useState("");
+
+   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+      setTaskTitle(e.target.value);
+   };
+
+   const handleNewTask = async () => {
+      if (taskTitle === "") {
+         return;
+      }
+
+      const task = {
+         title: taskTitle,
+         filter: taskFilter["filter"],
+         color: taskFilter["color"],
+      };
+
+      const result = await MakeRequest<{ message: string }>(
+         "tasks/add",
+         "POST",
+         {
+            ...task,
+         },
+         {
+            Authorization: `Bearer ${GetToken()}`,
+         }
+      );
+
+      if (!result.response?.success) return result.error;
+
+      setTasks([...tasks, task]);
+      setTaskTitle("");
+   };
+
+   const handleEnter = async (e: KeyboardEvent) => {
+      if (taskTitle === "") {
+         return;
+      }
+      if (e.key === "Enter") {
+         const task = {
+            title: taskTitle,
+            filter: taskFilter["filter"],
+            color: taskFilter["color"],
+         };
+
+         const result = await MakeRequest<{ message: string }>(
+            "tasks/add",
+            "POST",
+            {
+               ...task,
+            },
+            {
+               Authorization: `Bearer ${GetToken()}`,
+            }
+         );
+
+         if (!result.response?.success) return result.error;
+
+         setTasks([...tasks, task]);
+         setTaskTitle("");
+      }
+   };
+
    return (
       <div className="rounded-[15px] px-[1em] py-[5px] flex bg-white w-full overflow-hidden">
-         <div className="flex items-center border-r-2 border-[#e5e3e3] ">
-            <TaskFilter color="#3fd4f4" />
-            <TaskFilter color="#fd99af" />
-            <TaskFilter color="#fac608" />
+         <div className="flex items-center border-r-2 border-[#e5e3e3]">
+            <TaskFilter color="#3fd4f4" title="personal" setFilter={setTaskFilter} tasks={tasks} />
+            <TaskFilter color="#fd99af" title="freelance" setFilter={setTaskFilter} tasks={tasks} />
+            <TaskFilter color="#fac608" title="work" setFilter={setTaskFilter} tasks={tasks} />
          </div>
-         <input className="w-full text-[1.6rem] p-[0.5em] outline-none text-unactiveGrey" />
-         <i className="flex items-center text-[30px] pr-[10px] text-unactiveGrey hover:text-black transition-colors cursor-pointer">
+         <input
+            value={taskTitle}
+            onKeyDown={handleEnter}
+            onChange={handleInput}
+            className="w-full text-[1.6rem] p-[0.5em] outline-none text-unactiveGrey"
+            placeholder="Add your new target..."
+         />
+         <i
+            onClick={handleNewTask}
+            className="flex items-center text-[30px] pr-[10px] text-unactiveGrey hover:text-black transition-colors cursor-pointer"
+         >
             <FontAwesomeIcon icon={faPlus} />
          </i>
       </div>
--- todo-app-front-end/src/app/components/HomePage/components/MainContent/index.tsx
+++ todo-app-front-end/src/app/components/HomePage/components/MainContent/index.tsx
@@ -1,20 +1,44 @@
+import { useEffect, useState } from "react";
 import React from "react";
 import MainInput from "./components/MainInput";
+import TasksContainer from "./components/TasksContainer";
+import { TaskType } from "./interfaces/Task";
+import { MakeRequest } from "@/app/scripts/makeRequest";
+import { GetToken } from "@/app/scripts/auth";
 
-function index() {
+function Index() {
+   const [tasks, setTasks] = useState<TaskType[]>([]);
+
+   useEffect(() => {
+      const getTasks = async () => {
+         const result = await MakeRequest<{ tasks: TaskType[] }>("tasks", "GET", undefined, {
+            Authorization: `Bearer ${GetToken()}`,
+         });
+
+         if (!result.response?.success) return result.error;
+
+         setTasks(result.response.data.tasks);
+      };
+
+      getTasks();
+   }, []);
+
    return (
       <section className="w-full p-[2.4rem]">
          <h1 className="block text-[2rem] text-white mb-[5rem]">
             They don&apos;t know me son
             <span className="block text-[2.3rem] font-bold">
-               stay <strong className="underline">STRONG</strong>...
+               stay <strong>STRONG</strong>...
             </span>
          </h1>
-         <div className="w-full flex justify-center">
-            <MainInput />
+         <div className="w-full flex flex-col items-center gap-[40px]">
+            <MainInput setTasks={setTasks} tasks={tasks} />
+            <div className="p-[1em] w-full">
+               <TasksContainer tasks={tasks} setTasks={setTasks} />
+            </div>
          </div>
       </section>
    );
 }
 
-export default index;
+export default Index;
